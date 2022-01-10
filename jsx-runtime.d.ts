afede92216b893651d1e686cf1811a670d8642e8
olimot <olimot@gmail.com>
2022-01-10 20:25:48 +0900

    fix imports in jsx-runtime

--- jsx-runtime.d.ts
+++ jsx-runtime.d.ts
@@ -1,28 +1,27 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { JSXInternal, NodeChild, NodeChildren } from './src/jsx.d';
-import { FunctionComponent } from './src/main';
+import { JSX, NodeChild, NodeChildren, FunctionComponent } from './lib/main';
 
-export { JSXInternal as JSX };
+export { JSX };
 
 export function Fragment(attrs?: Record<string, any> & { children?: NodeChild }): DocumentFragment;
 
 export function jsx(
   type: string,
-  props: JSXInternal.HTMLAttributes & JSXInternal.SVGAttributes & Record<string, any> & { children?: NodeChild },
+  props: JSX.HTMLAttributes & JSX.SVGAttributes & Record<string, any> & { children?: NodeChild },
 ): Node;
 
 export function jsx<P>(type: FunctionComponent<P>, props: P & { children?: NodeChild }): Node;
 
 export function jsxs(
   type: string,
-  props: JSXInternal.HTMLAttributes & JSXInternal.SVGAttributes & Record<string, any> & { children?: NodeChild[] },
+  props: JSX.HTMLAttributes & JSX.SVGAttributes & Record<string, any> & { children?: NodeChild[] },
 ): Node;
 
 export function jsxs<P>(type: FunctionComponent<P>, props: P & { children?: NodeChild[] }): Node;
 
 export function jsxDEV(
   type: string,
-  props: JSXInternal.HTMLAttributes & JSXInternal.SVGAttributes & Record<string, any> & { children?: NodeChildren },
+  props: JSX.HTMLAttributes & JSX.SVGAttributes & Record<string, any> & { children?: NodeChildren },
 ): Node;
 
 export function jsxDEV<P>(type: FunctionComponent<P>, props: P & { children?: NodeChildren }): Node;
